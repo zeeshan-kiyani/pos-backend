3ed762cb73ea9640b49db00bb9c017ea532c231f
zeeshankiyani1 <zeeshan.kiyani@graana.com>
2021-09-18 00:30:37 +0500

    CRUD added

--- routes/api-inventory.js
+++ routes/api-inventory.js
@@ -1,5 +1,6 @@
 const router=require ('express-promise-router')();
 const sqlSequelize = require('../services/inventory/inventory-service');
+
 const getInventory=( req, res ) => {
     return sqlSequelize.getInventory().then((row) => {
         return res.status(200).json(row);
@@ -9,20 +10,34 @@ const getInventory=( req, res ) => {
 }
 const addInventory = (req,res) => {
     const {body} = req;
-    const product_name = body.product_name;
-    const quantity = body.quantity;
-    const price = body.price;
-    body.created_at = ''
     return sqlSequelize.addInventory(body).then((row) => {
-        return res.status(200).json(row);
+        return res.status(200).json({msg:'Inventory added successfully'});
+     }).catch(error =>{
+         return res.status(500,{error});
+     }); 
+}
+const updateInventory = (req,res) => {
+    const id = req.params.id;
+    const { body } = req;
+    body.created_at = new Date()
+    return sqlSequelize.updateInventory(id,body).then((row) => {
+        return res.status(200).json({msg:'Inventory updated successfully'});
      }).catch(error =>{
          return res.status(500,{error});
      }); 
 }
 
-router.get('/getInventory', getInventory)
-router.post('/addInventory', addInventory)
-// router.get('/updateInventory', getInventory)
-// router.get('/deleteInventory', getInventory)
-// router.get('/searchInventory', getInventory)
+const deleteInventory=( req, res ) => {
+    const id = req.params.id;
+    return sqlSequelize.deleteInventory(id).then((row) => {
+        return res.status(200).json({msg:'inventory deleted succesfully'});
+     }).catch(error =>{
+         return res.status(500,{error});
+     }); 
+}
+
+router.get('/getInventory', getInventory);
+router.post('/addInventory', addInventory);
+router.put('/updateInventory/:id', updateInventory);
+router.delete('/deleteInventory/:id', deleteInventory);
 module.exports = router;
--- services/inventory/inventory-service.js
+++ services/inventory/inventory-service.js
@@ -7,6 +7,20 @@ exports.getInventory = () => {
     });
 }
 exports.addInventory = (inventoryObj) => {
-    return db.sequelize.query(`insert into pos_backend.dbo.inventory('product_name','quantity','product_status','price','created_at') VALUES
-    ('${inventoryObj.product_name}','${inventoryObj.quantity}','${inventoryObj.product_status}','${inventoryObj.price}','${inventoryObj.created_at}')`);
+    return db.sequelize.query(`insert into dbo.inventory([product_name], [qantity], [price], [created_date]) VALUES
+    ('${inventoryObj.product_name}','${inventoryObj.quantity}','${inventoryObj.price}','${inventoryObj.created_date}')`)
 }
+
+exports.updateInventory = (id, inventoryObj) => {
+    return db.sequelize.query(`update dbo.inventory set 
+    [product_name] = '${inventoryObj.product_name}', 
+    [qantity] = '${inventoryObj.quantity}',
+    [product_status] = '${inventoryObj.product_status}', 
+    [price] = '${inventoryObj.price}',
+    [updated_date] = '${inventoryObj.updated_date}'
+    where id=${id}`)
+}
+exports.deleteInventory = (id) => {
+    return db.sequelize.query(`Delete from inventory where id=${id}`)
+}
+
