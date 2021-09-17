88097262488e4254fbfdd98f54a1e8d911a33830
zeeshankiyani1 <zeeshan.kiyani@graana.com>
2021-09-17 11:43:09 +0500

    add service added

--- services/inventory/inventory-service.js
+++ services/inventory/inventory-service.js
@@ -6,3 +6,7 @@ exports.getInventory = () => {
     return db.sequelize.query('select *from inventory',{ type: Sequelize.QueryTypes.SELECT
     });
 }
+exports.addInventory = (inventoryObj) => {
+    return db.sequelize.query(`insert into pos_backend.dbo.inventory('product_name','quantity','product_status','price','created_at') VALUES
+    ('${inventoryObj.product_name}','${inventoryObj.quantity}','${inventoryObj.product_status}','${inventoryObj.price}','${inventoryObj.created_at}')`);
+}
